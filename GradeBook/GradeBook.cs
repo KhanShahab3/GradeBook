4770490ee82e197a5777e15055ae46094380515a
KhanShahab3 <154610064+KhanShahab3@users.noreply.github.com>
2024-05-01 12:18:06 +0500

    Add project files.

---
+++ GradeBook/GradeBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Xml.Linq;
+namespace GradeBook;
+public class GradeBook
+{
+    private string courseName;
+    public string CourseName
+    {
+        get
+        {
+            return courseName;
+        }
+        set
+        {
+            courseName = value;
+        }
+    }
+
+   
+    public void DiplayMessage()
+    {
+        Console.WriteLine("Wellcome To Grade  Book..........",CourseName);
+        //Declaration of class contian the 
+        //a method name DisplayMessage the recall of this we
+        //need the object of class and call its method 
+    }
+    public void ClassAvergae()
+    {
+        int total=0;
+        int grade;
+        int gradeCounter=1;
+        double classAverage;
+        while (gradeCounter<=10) {
+            Console.WriteLine("Enter the Grade");
+            grade = Convert.ToInt32(  Console.ReadLine());
+            total=total+grade;
+            gradeCounter++;
+        }
+        classAverage = total / 10;
+        Console.WriteLine("total is " + total);
+        Console.WriteLine("class Average is " + classAverage);
+
+    }
+}
---
+++ GradeBook/GradeBookTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    public class GradeBookTest
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("\"please Enter the Course Name\"");
+            
+            GradeBook myGradeBook =new GradeBook();
+            //Console.WriteLine("intial courese name is '{0}'/n", myGradeBook.CourseName = "english");
+           
+            //Console.WriteLine("Enter the Course Name ");
+          
+        //myGradeBook.CourseName= Console.ReadLine();
+            myGradeBook.DiplayMessage();
+            myGradeBook.ClassAvergae(); 
+        }
+    }
+}
